bc0b5a592745684d4df9f5c1452cc971b4570fdd
Ahmed Youssef <ahmed.youssef.5522.52@gmail.com>
2025-09-22 02:26:39 +0300

    feat(fetching some data)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { useQuery } from "@tanstack/react-query"
+
 
 function App() {
 
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['todo'],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => res.json()
+        ),
+  })
+
+  if (error) {
+    return (
+      <div>
+        Something went wrong
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <div>
+        Loading.....
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-
+      {data.map((todo) => (
+        <div key={todo.id}>
+          <h1>ID: {todo.id}</h1>
+          <h2>Title: {todo.title}</h2>
+        </div>
+      ))}
     </div>
   )
 }
